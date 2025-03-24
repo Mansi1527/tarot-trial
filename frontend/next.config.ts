72d6d91afec6cdcb7b9068b599f987772ef89709
Mansi1527 <mansiy1527@icloud.com>
2025-03-24 18:27:44 +0530

    nextconfig change standalone

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  pageExtensions: ['tsx', 'ts'], // Ensures TSX/TS files are recognized
+  pageExtensions: ['tsx', 'ts'], 
+  trailingSlash: false,
+  output: "standalone",
+  // Ensures TSX/TS files are recognized
 };
 
 export default nextConfig;
