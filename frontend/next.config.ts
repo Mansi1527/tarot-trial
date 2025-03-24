ab6a389e5829dda7aa225ac69719ea7ed5a86cb2
Mansi1527 <mansiy1527@icloud.com>
2025-03-24 14:23:01 +0530

    build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['tsx', 'ts'], // Ensures TSX/TS files are recognized
 };
 
 export default nextConfig;
