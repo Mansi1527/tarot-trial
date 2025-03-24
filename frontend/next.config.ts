73c3d6191c1e00ba3ac870477d5ab0fe96bb2ce2
Mansi1527 <mansiy1527@icloud.com>
2025-03-24 19:43:49 +0530

    nextconfig change standalone

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: "export", // Enables static site generation
-  distDir: "out",   // Ensures output goes to 'out' folder
-  images: {
-    unoptimized: true, // Ensures compatibility with static hosting
-  },
-  trailingSlash: true, // Fixes broken links for static sites
+  output: 'standalone', // Enables standalone mode for deployment
 };
 
 module.exports = nextConfig;
